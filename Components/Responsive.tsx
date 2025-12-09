e46dc397b2e34eaf618d7f67c982b06768903766
sakshamOrdex <saksham.ojha.ordex@gmail.com>
2025-12-09 10:30:41 +0530

    Responsive Added

--- Components/Responsive.tsx
+++ Components/Responsive.tsx
@@ -1,13 +1,26 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { responsiveWidth, responsiveHeight, responsiveFontSize } from "react-native-responsive-dimensions";
 
 export default function Responsive() {
     return (
-        <View style={{ flex: 1, padding: 8 }}>
-            <View style={{ flex: 1, backgroundColor: 'red' }} />
-            <View style={{ flex: 1, backgroundColor: 'blue' }} />
+        <View style={styles.box}>
+            <View style={[{backgroundColor: 'red' },styles.box]} />
+            <View style={[{backgroundColor: 'blue' },styles.box]}> 
+                <Text style={[styles.text,{color:'white'}]}>
+                    This is Text
+                </Text>
+            </View>
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    box: {
+    width: responsiveWidth(80), // 80% of screen width
+    height: responsiveHeight(20),
+  },
+  text: {
+    fontSize: responsiveFontSize(3), // 3% of screen height
+  },
+})
